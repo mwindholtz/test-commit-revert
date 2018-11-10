37a2fbaa21903db8f5b7402a319e7449a00547dd
Mark Windholtz <mark@agiledna.com>
2018-11-10 15:05:19 -0500

    adds --sup

---
+++ lib/tcr/application.ex
@@ -0,0 +1,20 @@
+defmodule Tcr.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Tcr.Worker.start_link(arg)
+      # {Tcr.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Tcr.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
