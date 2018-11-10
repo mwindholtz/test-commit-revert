eab433abf4c5d4b38c68eb44bfe5b51a0d607862
Mark Windholtz <mark@agiledna.com>
2018-11-10 16:10:38 -0500

    adds output

--- lib/mix/tasks/tcr.ex
+++ lib/mix/tasks/tcr.ex
@@ -1,8 +1,18 @@
 defmodule Mix.Tasks.Tcr do
+  @moduledoc """
+  MIX_ENV=test mix tcr
+  """
   use Mix.Task
 
+  @preferred_cli_env :test
   @shortdoc "Runs Tcr"
-  def run(_) do
-    IO.inspect("HELLO")
+  def run(args) do
+    IO.inspect("RUNNING TESTS")
+    System.put_env("MIX_ENV", "test")
+
+    case Mix.Tasks.Test.run(~w|test args|) do
+      :ok -> IO.inspect("commit")
+      _ -> IO.inspect("revert")
+    end
   end
 end
