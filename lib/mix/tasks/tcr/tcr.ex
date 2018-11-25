306dd67f627b32fd9ed3eaa9323986d4aeb1bac7
Mark Windholtz <mark@agiledna.com>
2018-11-25 11:50:39 -0500

    working

--- lib/mix/tasks/tcr/tcr.ex
+++ lib/mix/tasks/tcr/tcr.ex
@@ -10,7 +10,7 @@ defmodule Mix.Tasks.Tcr.Tcr do
     IO.inspect("RUNNING TESTS #{inspect(args)}")
     System.put_env("MIX_ENV", "test")
 
-    case Mix.Tasks.Test.run(~w|test args|) do
+    case Mix.Tasks.Test.run(~w|test --trace|) do
       :ok ->
         IO.inspect("git commit -am working")
         System.cmd("git", ~w[commit -am working])
