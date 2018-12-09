7a9af95612b770bd61890b30f84a71af91fb96e6
Mark Windholtz <mark@agiledna.com>
2018-12-09 14:32:28 -0500

    rm application.ex

--- mix.exs
+++ mix.exs
@@ -16,8 +16,7 @@ defmodule Tcr.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Tcr.Application, []}
+      extra_applications: []
     ]
   end
 
